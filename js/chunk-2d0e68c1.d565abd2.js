1a29241fdeea93c595578ab4808f37bc83d1679a
unknown <zzharuk@gmail.com>
2021-08-13 20:24:21 +0300

    deploy

---
+++ js/chunk-2d0e68c1.d565abd2.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e68c1"],{"98e1":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("hr"),t("h5",[e._v("Movie Detail")])])}],l={},u=l,i=t("2877"),a=Object(i["a"])(u,c,r,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0e68c1.d565abd2.js.map
